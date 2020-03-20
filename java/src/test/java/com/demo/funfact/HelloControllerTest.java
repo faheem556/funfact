ddfaf91d07da4bdc389ea3daa735962c304359fd
Faheem Memon <memon.faheem@gmail.com>
2020-03-20 00:14:02 -0400

    Baseline demo code

---
+++ java/src/test/java/com/demo/funfact/HelloControllerTest.java
@@ -0,0 +1,34 @@
+package com.demo.funfact;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class HelloControllerTest {
+
+	@Autowired
+	private MockMvc mvc;
+
+	@Test
+	public void getIndex() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.TEXT_HTML))
+				.andExpect(status().isOk());
+    }
+    
+    public void getFact() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.get("/fact").accept(MediaType.TEXT_PLAIN))
+				.andExpect(status().isOk());
+	}
+
+}
